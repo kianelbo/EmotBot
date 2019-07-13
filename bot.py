1c67f1cc08b37fe6a7301e13550c0d7ffb310785
Kian Eliasi <kian.elbo@gmail.com>
2019-07-13 20:03:03 +0430

    deploy in heroku

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
-import sys
-from telebot import TeleBot
+import os
+import telebot
+from flask import Flask, request
 
 from emoticons import emote_keyboard, emote_dict, get_inline_results
 
-bot_token = sys.argv[1]
-bot = TeleBot(token=bot_token)
+TOKEN = os.environ['TOKEN']
+bot = telebot.TeleBot(token=TOKEN)
+server = Flask(__name__)
 
 
 @bot.inline_handler(lambda query: True)
@@ -36,5 +38,18 @@ def echo_all(message):
         bot.send_message(message.chat.id, 'unknown command :/', reply_markup=emote_keyboard)
 
 
-print('bot server started.')
-bot.polling()
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://emotbot.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
