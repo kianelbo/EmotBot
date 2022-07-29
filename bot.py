8105ebf70035cb83d0214c89e5f2e8c570f147ea
Kian Eliasi <kian.elbo@gmail.com>
2022-07-29 23:29:13 +0430

    Change the API library

--- bot.py
+++ bot.py
@@ -1,55 +1,65 @@
 import os
-import telebot
-from flask import Flask, request
 
-from emoticons import emote_keyboard, emote_dict, get_inline_results
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import CommandHandler, Filters, InlineQueryHandler, MessageHandler, Updater
+
+from emoticons import emotes_dict
+
 
 TOKEN = os.environ['TOKEN']
-bot = telebot.TeleBot(token=TOKEN)
-server = Flask(__name__)
 
 
-@bot.inline_handler(lambda query: True)
-def send_inline(inline_query):
-    try:
-        results = get_inline_results(inline_query.query)
-        bot.answer_inline_query(inline_query.id, results)
-    except Exception as e:
-        print(e)
+buttons = [InlineKeyboardButton(emote) for emote in emotes_dict.values()]
+keyboard = [buttons[x:x+3] for x in range(0, len(buttons), 3)]
+reply_markup = InlineKeyboardMarkup(keyboard)
+
+all_emote_results = [
+    InlineQueryResultArticle(i, emote, InputTextMessageContent(emote)) for i, emote in enumerate(emotes_dict.values())
+]
 
 
-@bot.message_handler(commands=['search'])
-def search_emote(message):
-    key = message.text[message.text.find(' ') + 1:]
-    if key in emote_dict:
-        bot.send_message(message.chat.id, emote_dict[key])
+def inline_query(update, context):
+    query = update.inline_query.query
+
+    if query in emotes_dict:
+        result = [InlineQueryResultArticle('1', emotes_dict[query], InputTextMessageContent(emotes_dict[query]))]
     else:
-        bot.send_message(message.chat.id, 'found nothing :(')
+        result = all_emote_results
+
+    update.inline_query.answer(result)
+
+def start(update, context):
+    update.message.reply_text("Just tap on the one you need!", reply_markup=reply_markup)
+
+def search(update, context):
+    query = update.message.text
+    key = query[query.find(' ') + 1:]
+    if key in emotes_dict:
+        update.message.reply_text(emotes_dict[key])
+    else:
+        update.message.reply_text("found nothing :(")
+
+def help(update, context):
+    update.message.reply_text('Help!')
+
+def non_command(update, context):
+    update.message.reply_text("unknown command :/", reply_markup=reply_markup)
 
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, "All you need to do is tap!", reply_markup=emote_keyboard)
+def main():
+    updater = Updater(TOKEN)
 
+    dp = updater.dispatcher
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    if message.text not in emote_dict.values():
-        bot.send_message(message.chat.id, 'unknown command :/', reply_markup=emote_keyboard)
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(InlineQueryHandler(inline_query))
+    dp.add_handler(CommandHandler("search", search))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, non_command))
 
-
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://emotbot.herokuapp.com/' + TOKEN)
-    return "!", 200
-
+    updater.start_webhook(listen="0.0.0.0", port=int(os.environ.get('PORT', 5000)), url_path=TOKEN)
+    updater.bot.setWebhook('https://emotbot.herokuapp.com/' + TOKEN)
+    updater.idle()
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    main()
--- emoticons.py
+++ emoticons.py
@@ -1,33 +1,19 @@
-from telebot import types
-
-emote_dict = {'😐': 'ಠ_ಠ',
-              '😣': '(>_<)',
-              '🙄': '( ⚆ _ ⚆ )',
-              '😳': '(⊙＿⊙)',
-              '😡': 'ᕙ(˵•̀෴•́˵)ᕗ',
-              '🙂': '(ツ)',
-              '😒': '(￢_￢)',
-              '☹': '(´◕︵◕`)',
-              '😄': '^‿^',
-              '😍': 'ヽ(♡‿♡)ノ',
-              '🤗': '༼ つ ◕_◕ ༽つ',
-              '😊': '(◔‿◔)',
-              '🤨': '( ͠° ͟ʖ ͡°)',
-              '🙋‍♂️': '⊂(◉‿◉)つ',
-              'lenny': '( ͡° ͜ʖ ͡°)',
-              '🖕': '( ︶︿︶)_╭∩╮',
-              'idk': '¯\\_(⊙︿⊙)_/¯', }
-
-emote_buttons = [types.KeyboardButton(emote) for emote in emote_dict.values()]
-emote_keyboard = types.ReplyKeyboardMarkup(row_width=3)
-emote_keyboard.add(*emote_buttons)
-
-inline_all_results = [types.InlineQueryResultArticle(i, emote, types.InputTextMessageContent(emote)) for i, emote in
-                      enumerate(emote_dict.values())]
-
-
-def get_inline_results(q):
-    if q in emote_dict:
-        return [types.InlineQueryResultArticle('1', emote_dict[q], types.InputTextMessageContent(emote_dict[q]))]
-    else:
-        return inline_all_results
+emotes_dict = {
+    '😐': 'ಠ_ಠ',
+    '😣': '(>_<)',
+    '🙄': '( ⚆ _ ⚆ )',
+    '😳': '(⊙＿⊙)',
+    '😡': 'ᕙ(˵•̀෴•́˵)ᕗ',
+    '🙂': '(ツ)',
+    '😒': '(￢_￢)',
+    '☹': '(´◕︵◕`)',
+    '😄': '^‿^',
+    '😍': 'ヽ(♡‿♡)ノ',
+    '🤗': '༼ つ ◕_◕ ༽つ',
+    '😊': '(◔‿◔)',
+    '🤨': '( ͠° ͟ʖ ͡°)',
+    '🙋‍♂️': '⊂(◉‿◉)つ',
+    'lenny': '( ͡° ͜ʖ ͡°)',
+    '🖕': '( ︶︿︶)_╭∩╮',
+    'idk': '¯\\_(⊙︿⊙)_/¯', 
+}
